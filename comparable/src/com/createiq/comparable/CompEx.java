5e2bcf7339be5e450c4e805c15e0505534b7a89f
madhuri <madhuritadepalli1234@gmail.com>
2022-04-09 17:25:55 +0530

    Adding programs

---
+++ comparable/src/com/createiq/comparable/CompEx.java
@@ -0,0 +1,11 @@
+package com.createiq.comparable;
+
+public class CompEx {
+	public static void main(String[] args) {
+		System.out.println("a". compareTo ("z"));
+		System.out.println("z".compareTo("a"));
+		System.out.println("a".compareTo("a"));
+		//System.out.println(2 compreTo (5));
+	}
+
+}
